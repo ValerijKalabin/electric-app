6e0dc4976c16715783e303ef298200cb726fd683
Valerij Kalabin <valerijkalabin@yandex.ru>
2022-03-19 01:48:32 +0700

    Create a Start element

--- src/components/Scheme/Scheme.js
+++ src/components/Scheme/Scheme.js
@@ -1,13 +1,10 @@
+import Start from '../../elements/Start/Start';
 import './Scheme.css';
 
 function Scheme() {
   return (
     <main className="scheme">
-      <button className="scheme__button" type="button">
-        <svg className="bi bi-file-earmark-ruled-fill" xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="#bbbbbb" viewBox="0 0 16 16">
-          <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM3 9h10v1H6v2h7v1H6v2H5v-2H3v-1h2v-2H3V9z"/>
-        </svg>
-      </button>
+      <Start />
     </main>
   );
 }
---
+++ src/elements/Start/Start.js
@@ -0,0 +1,55 @@
+import './Start.css';
+
+function Start() {
+  return (
+    <div className="start">
+      <h1 className="start__title">?</h1>
+      <ul className="start__list">
+        <li className="start__item start__item_auto">
+          <svg width="60" height="60" fill="transparent" stroke="#d5d832" strokeWidth="4" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="30" cy="30" r="26" />
+            <line x1="4" x2="22" y1="22" y2="22" />
+            <line x1="4" x2="22" y1="38" y2="38" />
+            <line x1="38" x2="56" y1="22" y2="22" />
+            <line x1="38" x2="56" y1="38" y2="38" />
+            <line x1="22" x2="38" y1="22" y2="14" />
+            <line x1="22" x2="38" y1="38" y2="30" />
+          </svg>
+        </li>
+        <li className="start__item start__item_box">
+          <svg width="60" height="60" fill="transparent" stroke="#d5d832" strokeWidth="4" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="30" cy="30" r="26" />
+          </svg>
+        </li>
+        <li className="start__item start__item_lamp">
+          <svg width="60" height="60" fill="transparent" stroke="#d5d832" strokeWidth="4" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="30" cy="30" r="26" />
+            <line x1="10" x2="50" y1="50" y2="10" />
+            <line x1="10" x2="50" y1="10" y2="50" />
+          </svg>
+        </li>
+        <li className="start__item start__item_socket">
+          <svg width="60" height="60" fill="transparent" stroke="#d5d832" strokeWidth="4" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="30" cy="30" r="26" />
+            <line x1="2" x2="16" y1="30" y2="30" />
+            <line x1="32" x2="58" y1="30" y2="30" />
+            <line x1="16" x2="26" y1="30" y2="20" />
+            <line x1="16" x2="26" y1="30" y2="40" />
+            <line x1="32" x2="42" y1="30" y2="20" />
+            <line x1="32" x2="42" y1="30" y2="40" />
+          </svg>
+        </li>
+        <li className="start__item start__item_switch">
+          <svg width="60" height="60" fill="transparent" stroke="#d5d832" strokeWidth="4" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="30" cy="30" r="26" />
+            <line x1="2" x2="20" y1="30" y2="30" />
+            <line x1="20" x2="40" y1="30" y2="20" />
+            <line x1="40" x2="58" y1="30" y2="30" />
+          </svg>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Start;
