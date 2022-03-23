adafc473bce7dfee2cad0efb2e021d189b0ebf54
Valerij Kalabin <valerijkalabin@yandex.ru>
2022-03-23 21:42:38 +0700

    Create a Add button

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,3 +1,4 @@
+import Add from '../../buttons/Add/Add';
 import Delete from '../../buttons/Delete/Delete';
 import Search from '../../buttons/Search/Search';
 import './List.css';
@@ -5,6 +6,7 @@ import './List.css';
 function List() {
   return (
     <main className="list">
+      <Add/>
       <Delete />
       <Search />
     </main>
