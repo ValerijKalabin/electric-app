312cf3ef6c483bd606d33ccbd3294b90c1076c8a
Valerij Kalabin <valerijkalabin@yandex.ru>
2022-04-03 13:00:55 +0700

    Add an image to the navigation button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -73,7 +73,9 @@ function Header({ elementList, isAllNavigationVisible }) {
                 "header__link header__link_elements"
               }
             >
-              +
+              <svg className="bi bi-plus" width="26" height="26" fill="#d5d832" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+              </svg>
             </NavLink>
           </li>
         </ul>
