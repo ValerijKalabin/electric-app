093fa67d3049448abaae38ee69e6c7255455ed92
Valerij Kalabin <valerijkalabin@yandex.ru>
2022-04-27 14:49:30 +0700

    Fix update drawing function

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -117,7 +117,7 @@ export const updateDrawing = (drawingId, elements) => {
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(elements)
+    body: JSON.stringify({elements})
   })
     .then(getResponseData);
 }
